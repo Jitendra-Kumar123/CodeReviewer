6c908c73ec4b2bfd2d89810e5185a4f0566c103a
Jitendra Kumar <jitendra0202006@gmail.com>
2026-01-21 20:06:11 +0530

    update deployed backend url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   //   }
   // }
 
-  const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_BASE = import.meta.env.VITE_API_URL || "https://codereviewerai-pkbx.onrender.com";
 
 async function reviewCode() {
   try {
